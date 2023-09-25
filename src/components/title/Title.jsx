9731efd1005dfb0d346db8242e98e7a332e458f0
pRemant <paudelremant45@gmail.com>
2023-09-25 18:22:29 +0545

    Title component created and css applied

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Title.css"
+
+const Title = () => {
+  return (
+    <>
+      <div className="title text-center">
+            <h2 className='mb-2'>Github Profile</h2>
+            <h5 className='mb-3'>Generate your Github Profile</h5>
+          </div>
+    </>
+  )
+}
+
+export default Title
