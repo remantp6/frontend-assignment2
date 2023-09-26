58e6720e0886740477ba67643c1bc2a70fde6c69
pRemant <paudelremant45@gmail.com>
2023-09-26 14:10:09 +0545

    routing configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SearchForm from './components/searchForm/SearchForm'
+import UserInfo from './components/userInfo/UserInfo'
 
 const App = () => {
   return (
     <>
-      <SearchForm/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchForm />} />
+        <Route path="/userInfo/:username" element={<UserInfo />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
