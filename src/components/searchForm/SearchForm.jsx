e83a6eaee791d2c0cb39bd4bab52bee604df9d21
pRemant <paudelremant45@gmail.com>
2023-09-26 14:04:06 +0545

    searching functionality added

--- src/components/searchForm/SearchForm.jsx
+++ src/components/searchForm/SearchForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchForm.css";
 import { Button, Container } from "react-bootstrap";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
@@ -6,17 +6,20 @@ import Form from "react-bootstrap/Form";
 import Title from "../title/Title";
 
 const SearchForm = () => {
+  const [username, setUsername] = useState("");
   return (
     <>
       <div className="search_form">
         <Container className="d-flex flex-column align-items-center justify-content-center vh-100">
-          <Title/>
+          <Title />
           <div className="search_form_field d-flex">
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Github Username"
-            >
-              <Form.Control type="text" placeholder="Github Username" />
+            <FloatingLabel controlId="floatingInput" label="Github Username">
+              <Form.Control
+                type="text"
+                placeholder="Github Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </FloatingLabel>
             <Button variant="warning">Generate</Button>{" "}
           </div>
